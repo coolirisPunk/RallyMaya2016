5db9e8fa580db78c7cd91ad925ca78b3aa4527ab
germanpunk <german@punkmkt.com>
2015-07-16 17:25:51 -0400

    Last changes

---
+++ app/src/main/java/com/punkmkt/rallymaya/fragments/diccionario_maya.java
@@ -0,0 +1,20 @@
+package com.punkmkt.rallymaya.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.punkmkt.rallymaya.R;
+
+/**
+ * Created by germanpunk on 02/03/15.
+ */
+public class Diccionario_maya extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.diccionario_maya_fragment,container,false);
+    }
+
+}
